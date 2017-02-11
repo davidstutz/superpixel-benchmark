74345b6dabfbb644f32543305e198b3c38f512af
David <davidstutz@web.de>
2017-02-11 18:31:43 +0100

    Updated documentation.

--- docs/html/search/classes_8.js
+++ docs/html/search/classes_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['medianblurdriver',['MedianBlurDriver',['../classMedianBlurDriver.html',1,'']]],
-  ['motionblurdriver',['MotionBlurDriver',['../classMotionBlurDriver.html',1,'']]]
+  ['motionblurdriver',['MotionBlurDriver',['../classMotionBlurDriver.html',1,'']]],
+  ['mss_5fopencv',['MSS_OpenCV',['../classMSS__OpenCV.html',1,'']]]
 ];
