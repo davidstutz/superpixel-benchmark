632236ec29971006465c17a4db9e5345fa8fc507
eudoxos <eu@doxos.eu>
2019-11-25 21:20:38 +0100

    fix compilation on ubuntu 18.04 (bionic) with default toolset

--- lib_dasp/lib_dasp/Plots.hpp
+++ lib_dasp/lib_dasp/Plots.hpp
@@ -14,6 +14,7 @@
 #include <Slimage/Paint.hpp>
 #include <Danvil/Color.h>
 #include <vector>
+#include <numeric>
 //----------------------------------------------------------------------------//
 namespace dasp {
 namespace plots {
